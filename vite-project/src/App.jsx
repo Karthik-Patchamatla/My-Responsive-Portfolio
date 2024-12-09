ef7f477384632604c13bf70da5a4a5bc1466b30c
Karthik-Patchamatla <pkarthikvarma830@gmail.com>
2024-12-10 00:16:26 +0530

    Small changes updated

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -2,6 +2,7 @@ import Header from "./components/Header";
 import Home from "./components/Home";
 import About from "./components/About";
 import Projects from "./components/Projects";
+import Contact from "./components/Contact";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Home />
       <About />
       <Projects />
+      <Contact/>
     </div>
   );
 }
